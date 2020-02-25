5e155b0760925dd4dade228cc1852e9c0b5bbec7
Zack McKenna <zackmckenna@gmail.com>
2020-02-25 15:04:49 -0500

    added navbar and login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LoginForm from './components/LoginForm';
+import NavbarTop from './components/TopNavbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+        <NavbarTop />
+        <LoginForm />
     </div>
   );
 }
