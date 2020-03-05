f14586a7aa9ad7dacb068c0977b0e1ac0a46037e
Zack McKenna <zackmckenna@gmail.com>
2020-03-05 16:37:45 -0500

    updating room function

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,8 @@ import { connect } from 'react-redux'
 import LoginForm from './LoginForm'
 import CreateAccountForm from './CreateAccountForm'
 import { MDBBtn, MDBInput, MDBBtnGroup } from 'mdbreact'
-import { setRoomName, dispatchRoomMessage } from '../redux/ducks/socket'
-import { setGame } from '../redux/ducks/session'
+import { setRoomName } from '../redux/ducks/socket'
+import { setGame, dispatchRoomMessage } from '../redux/ducks/session'
 
 const Home = (props) => {
   const [lobbyName, setLobbyName] = useState('')
@@ -28,7 +28,7 @@ const Home = (props) => {
   const handleChangeGameClick = (event) => {
     setSelectedGame(event.target.value)
     const gameToSet = props.games.games.filter(game => game.gameName === event.target.value)[0]
-    props.setGame(gameToSet)
+    props.setGame(gameToSet, props.socket.room)
   }
 
   const startGame = (currentGame) => {
@@ -42,8 +42,9 @@ const Home = (props) => {
     if(props.room) {
       return (
         <>
-          <h1>{selectedGame}</h1>
+          <h1>{props.session.selectedGame ? props.session.selectedGame.gameName : 'Choose a game'}</h1>
           <h2>Current Room: {props.room}</h2>
+          {props.session.host ? <p>You are the host</p> : null }
           <MDBBtnGroup>
             {props.games.games.map(game => {
               return (
@@ -54,21 +55,21 @@ const Home = (props) => {
               )
             })}
           </MDBBtnGroup>
-          <MDBBtn onClick={() => startGame(props.games.selectedGame)}>Start Game</MDBBtn>
+          <MDBBtn onClick={() => startGame(props.session.selectedGame)}>Start Game</MDBBtn>
           <h2>Users</h2>
-          {props.games.selectedGame ? <h4>Must have at least {props.games.selectedGame.minPlayers} to play.</h4> : ''}
+          {/* {props.session.selectedGame ? <h4>Must have at least {props.session.selectedGame.minPlayers} to play.</h4> : ''} */}
           {props.usersInRoom ? props.usersInRoom.map((client, index) => <p key={index}>{client.username}</p>) : null}
           <h2>send users message</h2>
           <MDBBtn onClick={() => handleSendMessage()}>Send Message To Room</MDBBtn><MDBInput value={message} onChange={event => handleMessageChange(event)} label="message" icon="lock" group type="email" validate />
           <h3>messages</h3>
-          {props.socket.messages.map((message, index) => <li key={index}>{message}</li>)}
+          {props.session.messages ? props.session.messages.map((message, index) => message ? <p key={index}>{message.user}: {message.message}</p> : null) : null }
         </>
       )
     } else if (!props.room){
       return (
         <>
           <h1>Home page. User: {props.login.user.username} </h1>
-          <MDBBtn onClick={() => handleCreateRoom()}>Create Room</MDBBtn><MDBInput value={lobbyName} onChange={event => handleRoomNameChange(event)} label="create a room" icon="lock" group type="email" validate />
+          <MDBBtn onClick={() => handleCreateRoom()}>Create/Join Room</MDBBtn><MDBInput value={lobbyName} onChange={event => handleRoomNameChange(event)} label="create a room" icon="lock" group type="email" validate />
           <h3>Join Lobby</h3>
           <h2>send users message</h2>
           <MDBBtn onClick={() => handleSendMessage()}>Send Message To Room</MDBBtn><MDBInput value={message} onChange={event => handleMessageChange(event)} label="type a message" icon="lock" group type="email" validate />
@@ -99,7 +100,8 @@ const mapStateToProps = state => {
     room: state.socket.room,
     socket: state.socket,
     usersInRoom: state.socket.currentClientsInRoom,
-    games: state.games
+    games: state.games,
+    session: state.session
   }
 }
 
--- src/redux/ducks/session.js
+++ src/redux/ducks/session.js
@@ -5,13 +5,25 @@ export const START_GAME_SUCCESS = 'skeleton-card/redux/ducks/session/START_GAME_
 export const START_GAME_FAIL = 'skeleton-card/redux/ducks/session/START_GAME_FAIL'
 
 export const DISPATCH_GAME_TO_SOCKET = 'server/DISPATCH_GAME_TO_SOCKET'
+export const DISPATCH_GAME_TO_CLIENTS = 'skeleton-card/redux/ducks/session/DISPATCH_GAME_TO_CLIENTS'
+
+export const DISPATCH_ROOM_MESSAGE_TO_CLIENTS = 'skeleton-card/redux/ducks/session/DISPERSE_ROOM_MESSAGE_TO_CLIENTS'
+export const DISPATCH_ROOM_MESSAGE_TO_SOCKET = 'server/DISPATCH_ROOM_MESSAGE_TO_SOCKET'
+export const LOAD_EXISTING_MESSAGES = 'skeleton-card/redux/ducks/session/LOAD_EXISTING_MESSAGES'
 
 export const SET_GAME = 'skeleton-card/redux/ducks/session/SET_GAME'
 export const SET_HOST = 'skeleton-card/redux/ducks/session/SET_HOST'
 export const SET_USERS = 'skeleton-card/redux/ducks/session/SET_HOST'
 
-export default function reducer(state = { users: [], host: false }, action) {
+export default function reducer(state = { messages: [], users: [], host: false, selectedGame: null }, action) {
   switch (action.type) {
+  case DISPATCH_GAME_TO_CLIENTS:
+    return { ...state, selectedGame: action.payload }
+  case DISPATCH_ROOM_MESSAGE_TO_CLIENTS:
+    console.log(action.payload)
+    return { ...state, messages: [...state.messages, action.payload] }
+  case LOAD_EXISTING_MESSAGES:
+    return { ...state, messages: action.payload }
   case SET_GAME:
     return { ...state, selectedGame: action.payload }
   case SET_HOST:
@@ -27,18 +39,38 @@ export default function reducer(state = { users: [], host: false }, action) {
   }
 }
 
-export const setGame = (game) => dispatch => {
+export const dispatchGameToClients = (game) => dispatch => {
+  dispatch({
+    type: DISPATCH_GAME_TO_CLIENTS,
+    payload: game
+  })
+}
+
+export const dispatchRoomMessage = (roomName, message) => {
+  return {
+    type: DISPATCH_ROOM_MESSAGE_TO_SOCKET,
+    payload: {
+      roomName: roomName,
+      message: message
+    }
+  }
+}
+
+export const setGame = (game, room) => dispatch => {
   dispatch ({
     type: SET_GAME,
     payload: game
   })
-  dispatch(dispatchSelectedGameToSocket(game))
+  dispatch(dispatchSelectedGameToSocket(game, room))
 }
 
-export const dispatchSelectedGameToSocket = game => {
+export const dispatchSelectedGameToSocket = ( game, room ) => {
   return {
     type: DISPATCH_GAME_TO_SOCKET,
-    payload: game
+    payload: {
+      game: game,
+      room: room
+    }
   }
 }
 
